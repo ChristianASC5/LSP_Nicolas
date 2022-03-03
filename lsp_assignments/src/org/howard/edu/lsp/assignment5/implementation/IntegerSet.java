36e35bf99c6eaf292bad301a7408944eb101790d
ChristianASC5 <cnicolas3411@gmail.com>
2022-03-03 18:25:42 -0500

    Wrote and added javadocs for some functions

--- lsp_assignments/src/org/howard/edu/lsp/assignment5/implementation/IntegerSet.java
+++ lsp_assignments/src/org/howard/edu/lsp/assignment5/implementation/IntegerSet.java
@@ -16,9 +16,7 @@ public class IntegerSet {
 	private ArrayList<Integer> set;
 	
 	/**
-	 * 
 	 * This is the default constructor for an IntegerSet object
-	 * 
 	 * Initializes set attribute
 	 */
 	public IntegerSet() {
@@ -26,18 +24,15 @@ public class IntegerSet {
 	}
 	
 	/**
-	 * 
 	 * Clears internal representation of the set
-	 * 
 	 */
 	public void clear() {
 		set.clear();
 	}
 	
 	/**
-	 * 
 	 * Returns the length of the set
-	 * 
+	 * @return
 	 */
 	public int length() {
 		return set.size();
@@ -46,11 +41,11 @@ public class IntegerSet {
 	 
 	/**
 	 * 
-	 * Returns true if the 2 sets are equal, false otherwise; 
+	 * Returns true if the 2 sets are equal, false otherwise. 
 	 * Two sets are equal if they contain all of the same values in ANY order.
 	 * 
-	 * @param b
-	 * 
+	 * @param b the IntegerSet this set is being compared to
+	 * @return true if this IntegerSet is equal to IntegerSet b, false otherwise
 	 */
 	public boolean equals(IntegerSet b) {
 		if (length() != b.length()){
@@ -63,11 +58,14 @@ public class IntegerSet {
 				}
 			}
 		}
-		
-		return true;
-		
+		return true;	
 	}
 	
+	/**
+	 * Returns true if the set contains the value, otherwise false
+	 * @param value
+	 * @return
+	 */
 	public boolean contains(int value) {
 		return (Collections.binarySearch(set, value) != -1);
 	}
