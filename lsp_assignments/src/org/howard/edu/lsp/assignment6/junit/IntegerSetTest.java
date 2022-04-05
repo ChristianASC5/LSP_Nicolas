1142800e6c4d30b4d07c011e3bd84a7c2cbcc852
ChristianASC5 <cnicolas3411@gmail.com>
2022-04-04 22:19:41 -0400

    Added other integer set values

--- lsp_assignments/src/org/howard/edu/lsp/assignment6/junit/IntegerSetTest.java
+++ lsp_assignments/src/org/howard/edu/lsp/assignment6/junit/IntegerSetTest.java
@@ -1,18 +1,66 @@
 package org.howard.edu.lsp.assignment6.junit;
+
+import org.howard.edu.lsp.assignment6.integerset.*;
+import static org.junit.jupiter.api.Assertions.*;
 import junit.framework.*;
 
-public class IntegerSetTest {
+import org.junit.jupiter.api.Test;
+
+class IntegerSetTest extends TestCase{
+	IntegerSet emptySet1;
+	IntegerSet emptySet2;
+	IntegerSet oneValueSet;
+	IntegerSet multiValueSet1;
+	IntegerSet multiValueSet2;
+	IntegerSet multiValueSet3;
 	
 	public IntegerSetTest() {}
 	
+	protected void setUp() {
+		//Empty integer sets
+		emptySet1 = new IntegerSet();
+		emptySet2 = new IntegerSet();
+		
+		//Integer set with one value
+		oneValueSet = new IntegerSet();
+		
+		//Integer set
+		multiValueSet1 = new IntegerSet();
+		multiValueSet1.add(1);
+		multiValueSet1.add(2);
+		multiValueSet1.add(3);
+		multiValueSet1.add(4);
+		multiValueSet1.add(5);
+		
+		multiValueSet2 = new IntegerSet();
+		multiValueSet2.add(1);
+		multiValueSet2.add(2);
+		multiValueSet2.add(3);
+		multiValueSet2.add(4);
+		multiValueSet2.add(5);
+		
+		multiValueSet3 = new IntegerSet();
+		
+	}
+	
+	protected void tearDown() {}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test	
 	public void testClear() {
 		
 	}
 	
+	@Test
 	public void testLength() {
 		
 	}
 	
+	@Test
 	public void testEquals() {
 		
 	}
@@ -48,4 +96,5 @@ public class IntegerSetTest {
 	public void testDiff() {
 		
 	}
+
 }
